a47ef38594a39fffd86a5473b593d666382db2c2
Shao0203 <nzsyl0203@gmail.com>
2025-08-07 21:29:55 +0800

    streamlit playground

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('start...')
+import streamlit as st
+import pandas as pd
+
+
+st.write('## *Hello world* !!!')
+
+st.image('./Rabbit.jpeg', width=200)
+
+['apple', 'pear', 'orange']
+
+{'name': 'Tom', 'age': 20, 'gender': 'male', 'hobby': 'sports'}
+
+2**10
+
+df = pd.DataFrame({
+    'name': ['Bob', 'Candy', 'Dave', 'Eddy'],
+    'id': ['01', '02', '03', '04'],
+    'class': ['one', 'two', 'three', 'four'],
+    'grade': [60, 70, 80, 90],
+})
+
+df
+st.divider()
+st.dataframe(df)
+st.table(df)
